026dc0dee29302e906342e7cfbfaf4c500eb690f
Piyush Singh <piyush.0966@gmail.com>
2018-05-22 13:06:39 +0530

    heruko update

--- server.js
+++ server.js
@@ -5,11 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/src'));
 
 app.get('/', function(req,res) {
-    
-res.send('hello world');
+    res.sendFile(__dirname +'/index.html');  
 });
 
 // Start the app by listening on the default Heroku port
